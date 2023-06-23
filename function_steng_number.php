701dd3ce9285286c9602c108ce0470b01a66fe72
The code <134747320+the-code1@users.noreply.github.com>
2023-06-23 20:58:06 +0300

    Create function_steng_number.php

---
+++ function_steng_number.php
@@ -0,0 +1,9 @@
+///لەبەر ئەوەی strict چالاک نەکراوە "5 ڕۆژ" دەگۆڕدرێت بۆ int(5)، و 10 دەگەڕێنێتەوە
+// since strict is enabled and "5 days" is not an integer, an error will be thrown
+<?php
+function addNumbers(int $a, int $b) {
+  return $a + $b;
+}
+echo addNumbers(5, "5 days"); 
+// since strict is NOT enabled "5 days" is changed to int(5), and it will return 10
+?>
