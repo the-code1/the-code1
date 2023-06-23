14a7e798e2d331b6bdd4f7b01c15076f160b5ae2
The code <134747320+the-code1@users.noreply.github.com>
2023-06-23 20:48:23 +0300

    Create two arguments.php

---
+++ two arguments.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function familyName($fname, $year) {
+  echo "name $fname age $year <br>";
+}
+
+familyName("7ama","2005");
+familyName("Stale","1978");
+familyName("Kai Jim","1983");
+?>
+
+</body>
+</html>
