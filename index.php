232b64f38fd1d083f9debc262cd9f1cff2096ec8
The code <134747320+the-code1@users.noreply.github.com>
2023-06-23 20:10:31 +0300

    Create index.php
    php; varible  helo word nusen la html

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/// echo "hello word"
+echo "hello word";
+
+$name= "7ama";
+$num = 12;
+$boleean = true; ///false
+$k = '7ama';
+echo $name;
+
+
+?>
+<!--nusen la naw html-->
+<h1><?php echo $name ?></h1>
