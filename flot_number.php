a6e4da49102a9e93f3189ea0c67cae46a153885b
The code <134747320+the-code1@users.noreply.github.com>
2023-06-23 21:54:01 +0300

    flot_number.php

--- flot_number.php
+++ flot_number.php
@@ -4,3 +4,12 @@ function addNumbers(float $a, float $b) : float {
 }
 echo addNumbers(1.2, 5.2); 
 ?>
+///jeawaze
+
+<?php declare(strict_types=1); // strict requirement
+function addNumbers(float $a, float $b) :int {
+  return (int) ($a + $b);
+}
+echo addNumbers(1.2, 5.2); 
+?>
+
